57b6604cb76b28a342b310e34301fa03e66051ea
Najmiddin <nazarmatovn03@gmail.com>
2023-10-09 20:13:01 +0200

    CompareAndSwapLock

---
+++ src/CompareAndSwapLock.java
@@ -0,0 +1,15 @@
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class CompareAndSwapLock{
+    private AtomicBoolean atomicLocked=new AtomicBoolean(false);
+
+    public void unlock(){
+        this.atomicLocked.set(false);
+    }
+
+    public void lock(){
+        while(!this.atomicLocked.compareAndSet(false,true)){
+            //busy wait - until compareAndSet succeeds
+        }
+    }
+}
